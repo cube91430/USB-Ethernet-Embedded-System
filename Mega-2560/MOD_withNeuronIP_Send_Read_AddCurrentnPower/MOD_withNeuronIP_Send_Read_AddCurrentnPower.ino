4cb317f24673462193a0b1d209f444a0515fafd4
wakatobi <80981936+cube91430@users.noreply.github.com>
2025-07-25 16:10:12 +0700

    Add files via upload

--- Mega-2560/MOD_withNeuronIP_Send_Read_AddCurrentnPower/MOD_withNeuronIP_Send_Read_AddCurrentnPower.ino
+++ Mega-2560/MOD_withNeuronIP_Send_Read_AddCurrentnPower/MOD_withNeuronIP_Send_Read_AddCurrentnPower.ino
@@ -1,13 +1,15 @@
-//This is a Dummy_Data from the Arduino Mega
-
 #include <Ethernet.h>
 #include <ArduinoModbus.h>
 #include <DHT.h>
 #include <SPI.h>
-
+#include <EEPROM.h>
+#include <PZEM004Tv30.h>
 
 #define DHTTYPE DHT22
 DHT dht1(2, DHTTYPE);
+
+PZEM004Tv30 pzem1(&Serial1);
+PZEM004Tv30 pzem2(&Serial2);
 
 byte mac[] = { 0xBC, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  // match your MAC address
 IPAddress ip(192, 168, 10, 177);                       // match your Arduino IP
@@ -29,7 +31,6 @@ unsigned long duration = 20 * 60 * 1000UL;  // 20 minutes in milliseconds
 
 // Simulation variables
 int voltage, current, powerFactor, power, energy_kWh, frequency;
-
 int voltage1, current1, powerFactor1, power1, energy_kWh1, frequency1;
 int temperature, humidity;
 
@@ -38,10 +39,10 @@ const int relay1 = 45;
 const int relay2 = 46;
 const int relay3 = 47;
 
-const int relay4;
-const int relay5;
-const int relay6;
-const int relay7;
+const int relay4 = 43;
+const int relay5 = 42;
+const int relay6 = 41;
+const int relay7 = 40;
 
 void resetSimulation() {
   startTime = millis();
@@ -85,14 +86,22 @@ void simulateNormalBehavior() {
 }
 
 void updateHoldingRegister() {
-  modbusTCP.holdingRegisterWrite(0, temperature);     //1!400001
-  modbusTCP.holdingRegisterWrite(3, humidity);        //1!400004
-  modbusTCP.holdingRegisterWrite(10, voltage);        //1!400011
-  modbusTCP.holdingRegisterWrite(11, current);        //1!400012
-  modbusTCP.holdingRegisterWrite(12, power);          //1!400013
-  modbusTCP.holdingRegisterWrite(13, powerFactor);    //1!400014
-  modbusTCP.holdingRegisterWrite(14, frequency);      //1!400015
-  modbusTCP.holdingRegisterWrite(15, energy_kWh);     //1!400016
+  modbusTCP.holdingRegisterWrite(0, temperature);             //1!400001
+  modbusTCP.holdingRegisterWrite(3, humidity);                //1!400004
+
+  modbusTCP.holdingRegisterWrite(10, pzem1.voltage());        //1!400011
+  modbusTCP.holdingRegisterWrite(11, pzem1.current());        //1!400012
+  modbusTCP.holdingRegisterWrite(12, pzem1.power());          //1!400013
+  modbusTCP.holdingRegisterWrite(13, pzem1.pf());             //1!400014
+  modbusTCP.holdingRegisterWrite(14, pzem1.frequency());      //1!400015
+  modbusTCP.holdingRegisterWrite(15, pzem1.energy());         //1!400016
+
+  modbusTCP.holdingRegisterWrite(16, pzem2.voltage());        //1!400017
+  modbusTCP.holdingRegisterWrite(17, pzem2.current());        //1!400018
+  modbusTCP.holdingRegisterWrite(18, pzem2.power());          //1!400019
+  modbusTCP.holdingRegisterWrite(19, pzem2.pf());             //1!400020
+  modbusTCP.holdingRegisterWrite(20, pzem2.frequency());      //1!400021
+  modbusTCP.holdingRegisterWrite(21, pzem2.energy());         //1!400022
 }
 
 void updateCoil() {
@@ -100,6 +109,12 @@ void updateCoil() {
   digitalWrite(relay1, modbusTCP.coilRead(1));        //1!0002
   digitalWrite(relay2, modbusTCP.coilRead(2));        //1!0003
   digitalWrite(relay3, modbusTCP.coilRead(3));        //1!0004
+
+  digitalWrite(relay4, modbusTCP.coilRead(4));        //1!0005
+  digitalWrite(relay5, modbusTCP.coilRead(5));        //1!0006
+  digitalWrite(relay6, modbusTCP.coilRead(6));        //1!0007
+  digitalWrite(relay7, modbusTCP.coilRead(7));        //1!0008
+
 }
 
 void DiscreteInput_Write(int coil, int arr){
@@ -121,6 +136,8 @@ void WriteDiscreteInput() {
 void setup() {
   Serial.begin(9600);
   dht1.begin();
+  Serial1.begin();
+  Serial2.begin();
 
   Ethernet.begin(mac, ip);
   if (!modbusTCP.begin()) {
@@ -129,9 +146,9 @@ void setup() {
       ;
   }
   Serial.println("Modbus TCP Server started");
-  modbusTCP.configureHoldingRegisters(0x00, 100);
-  modbusTCP.configureCoils(0x00, 40);
-  modbusTCP.configureDiscreteInputs(0x00, 40);
+  modbusTCP.configureHoldingRegisters(0x00, 50);
+  modbusTCP.configureCoils(0x00, 11);
+  modbusTCP.configureDiscreteInputs(0x00, 11);
 
   client.connect(neuronEX, NeuronPort);
   server.begin();
@@ -140,6 +157,10 @@ void setup() {
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
+  pinMode(relay5, OUTPUT);
+  pinMode(relay6, OUTPUT);
+  pinMode(relay7, OUTPUT);
 
   delay(2000);    //Delay from start
 
@@ -150,10 +171,9 @@ void loop() {
   temperature = dht1.readTemperature();
   humidity = dht1.readHumidity();
 
-  simulateNormalBehavior();
-
-  power = voltage * current * powerFactor;
-  energy_kWh += (power / 1000.0) * (1.0 / 3600.0);  // kWh increment per second
+  //simulateNormalBehavior();
+  //power = voltage * current * powerFactor;
+  //energy_kWh += (power / 1000.0) * (1.0 / 3600.0);  // kWh increment per second
 
   EthernetClient client = server.available();
 
